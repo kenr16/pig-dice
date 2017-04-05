7b52ec8ea8d75be49f2120781dc895e7133b661e
Jennifer Kinsey and Ken Rutan <jennifer@>
2017-04-05 15:06:56 -0700

    Added a much harder AI program.

--- js/scripts.js
+++ js/scripts.js
@@ -51,8 +51,27 @@ function resetFields() {
   $("#pot1, #roll1, #pot2, #roll2").text(0);
 }
 
-
 $(function(){
+
+//**********************************************
+  //Failed attemp to implement a modal:
+  // Get the modal
+  // var modal = document.getElementById('myModal');
+  // // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[0];
+  //     modal.style.display = "block";  //This will open the modal
+  //     $("#modalText").text("Hello");
+  // // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //     modal.style.display = "none";
+  // }
+  // // When the user clicks anywhere outside of the modal, close it
+  // window.onclick = function(event) {
+  //     if (event.target == modal) {
+  //         modal.style.display = "none";
+  //     }
+  // }
+//*************************************************
 
   var player1Name = "Player 1";
   var player2Name = "Player 2";
@@ -81,6 +100,7 @@ $(function(){
       $("#roll1").text(dieRoll);
       $(".btn").toggleClass("hidden");
       $(".col-xs-5").toggleClass("selected");
+      alert("You have rolled a 1.");
       aiScript();             //Disable for single player.
     } else {
       $("#roll1").text(dieRoll);
@@ -104,16 +124,16 @@ $(function(){
       aiScript();
     }
   });
-
-
-// ************************************************
-// Dumb AI Script
-// ************************************************
-  var aiScript = function() {
-    alert("Player 2's turn");
-    var dieRolls = [];
-      for (var i = 0; i < 2; i ++) {
+  // ************************************************
+  // Smart AI Script
+  // ************************************************
+    var aiScript = function() {
+      alert(Player2.name + "'s turn!");
+      var dieRolls = [];
+      while (Player2.pot < 20 || (Player1.total + 10) >= (Player2.total + Player2.pot)) {
         var dieRoll = parseInt(getRandomArbitrary());
+        alert(Player2.name + " has rolled a "+ dieRoll + "\nCurrent Pot: " + Player2.pot);
+
         if (Player2.checkOne(dieRoll) === true) {
           $("#pot2").text(0);
           $("#roll2").text(dieRoll);
@@ -128,27 +148,75 @@ $(function(){
           $("#pot2").text(Player2.pot);
         }
         dieRolls.push(dieRoll);
-        if (i == 2) {
-          alert("The AI has finished with the following rolls: " + dieRolls);
-        }
       }
-    Player2.hold();
-    $("#totalscore2").text(Player2.total);
-    resetFields();
-    $(".btn").toggleClass("hidden");
-    $(".col-xs-5").toggleClass("selected");
+      alert("The AI has finished with the following rolls: " + dieRolls);
 
-    if (Player2.victory() === true) {
-      var delayMillis = 200; //1 second
-      setTimeout(function() {
-        alert("Victory for " + Player2.name + "!");
-      }, delayMillis);
+      Player2.hold();
+      $("#totalscore2").text(Player2.total);
+      resetFields();
       $(".btn").toggleClass("hidden");
       $(".col-xs-5").toggleClass("selected");
-    }
 
-  }
-});
+      if (Player2.victory() === true) {
+        var delayMillis = 200; //1 second
+        setTimeout(function() {
+          alert("Victory for " + Player2.name + "!");
+        }, delayMillis);
+        $(".btn").toggleClass("hidden");
+        $(".col-xs-5").toggleClass("selected");
+      }
+
+    }
+  });
+
+  // ************************************************
+  // End Smart AI Script
+  // ************************************************
+
+// ************************************************
+// Dumb AI Script
+// ************************************************
+
+//   var aiScript = function() {
+//     alert("Player 2's turn");
+//     var dieRolls = [];
+//       for (var i = 0; i < 2; i ++) {
+//         var dieRoll = parseInt(getRandomArbitrary());
+//         if (Player2.checkOne(dieRoll) === true) {
+//           $("#pot2").text(0);
+//           $("#roll2").text(dieRoll);
+//           $(".btn").toggleClass("hidden");
+//           $(".col-xs-5").toggleClass("selected");
+//           alert("The AI has rolled a 1");
+//           dieRolls.push(dieRoll);
+//           return;
+//         } else {
+//           $("#roll2").text(dieRoll);
+//           Player2.roll(dieRoll);
+//           $("#pot2").text(Player2.pot);
+//         }
+//         dieRolls.push(dieRoll);
+//         if (i == 2) {
+//           alert("The AI has finished with the following rolls: " + dieRolls);
+//         }
+//       }
+//     Player2.hold();
+//     $("#totalscore2").text(Player2.total);
+//     resetFields();
+//     $(".btn").toggleClass("hidden");
+//     $(".col-xs-5").toggleClass("selected");
+//
+//     if (Player2.victory() === true) {
+//       var delayMillis = 200; //1 second
+//       setTimeout(function() {
+//         alert("Victory for " + Player2.name + "!");
+//       }, delayMillis);
+//       $(".btn").toggleClass("hidden");
+//       $(".col-xs-5").toggleClass("selected");
+//     }
+//
+//   }
+// });
 
 // ************************************************
 // End Dumb AI Script
